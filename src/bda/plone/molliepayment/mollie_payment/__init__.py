d65f4e807b89ef513513de4e8247a14324cd56fc
André Gonçalves <andreslb1@gmail.com>
2015-12-04 15:16:05 +0100

    Fix failed page

--- src/bda/plone/molliepayment/mollie_payment/__init__.py
+++ src/bda/plone/molliepayment/mollie_payment/__init__.py
@@ -329,6 +329,27 @@ class MolliePayFinalized(BrowserView):
     @property
     def shopmaster_mail(self):
         return shopmaster_mail(self.context)
+
+    def get_header_image(self, is_ticket=False):
+        if is_ticket:
+            folder = self.context
+            if folder.portal_type == "Folder":
+                contents = folder.getFolderContents({"portal_type": "Image", "Title":"tickets-header"})
+                if len(contents) > 0:
+                    image = contents[0]
+                    url = image.getURL()
+                    scale_url = "%s/%s" %(url, "@@images/image/large")
+                    return scale_url
+        else:
+            brains = self.context.portal_catalog(Title="webwinkel-header", portal_type="Image")
+            if len(brains) > 0:
+                brain = brains[0]
+                if brain.portal_type == "Image":
+                    url = brain.getURL()
+                    scale_url = "%s/%s" %(url, "@@images/image/large")
+                    return scale_url
+
+            return ""
         
 #
 # Payment failed
@@ -339,6 +360,8 @@ class MolliePayFailed(BrowserView):
     @property
     def shopmaster_mail(self):
         return shopmaster_mail(self.context)
+
+
 
 class MollieError(Exception):
     """Raised if Mollie Payment return an error.
